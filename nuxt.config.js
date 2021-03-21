733d05b99128aabb031e940a7427881ea2b9b6c1
Andrii Moisol <andrii.moisol@gmail.com>
2021-03-21 19:57:56 +0200

    WIP

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: './_nuxt/'
+    publicPath: '/klevo/'
   },
 
   styleResources: {
@@ -54,5 +54,6 @@ export default {
     dir: 'docs'
   },
 
-  target: 'static'
+  target: 'static',
+  ssr: false
 }
